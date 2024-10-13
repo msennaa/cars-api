09d614ac83e9af405ff4b212c2f802af25da3189
Matheus Sena <senamatheusjob@gmail.com>
2024-10-13 20:36:52 -0300

    test: should not create an account with invalid cpf

--- test/signup.test.ts
+++ test/signup.test.ts
@@ -22,3 +22,15 @@ test('should create a driver new account successfully', async () => {
     const output = await signup(input)
     expect(output.accountId).toBeDefined();
 })
+
+
+test('should not create a new account with invalid cpf', async () => {
+    const input = {
+        name: 'any name',
+        email: `any_email${Math.random()}@mail.com`,
+        cpf: '0000000000',
+        isPassenger: true
+    }
+    const output = await signup(input)
+    expect(output).toBe(-1)
+})
