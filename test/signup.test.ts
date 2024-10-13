a9a3f9c10d5ef6fa79fb02f67ca42e1a2f03af13
Matheus Sena <senamatheusjob@gmail.com>
2024-10-13 20:36:14 -0300

    test: should create a driver account successfully

--- test/signup.test.ts
+++ test/signup.test.ts
@@ -10,3 +10,15 @@ test('should create a passenger new account successfully', async () => {
     const output = await signup(input)
     expect(output.accountId).toBeDefined();
 })
+
+test('should create a driver new account successfully', async () => {
+    const input = {
+        name: 'any name',
+        email: `any_email${Math.random()}@mail.com`,
+        cpf: '97456321558',
+        isDriver: true,
+        carPlate: 'AAA0000'
+    }
+    const output = await signup(input)
+    expect(output.accountId).toBeDefined();
+})
