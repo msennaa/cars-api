5d6c3728a1d6c9c9bcca0adb65af9fd8ff4ae579
Matheus Sena <senamatheusjob@gmail.com>
2024-10-14 05:27:52 -0300

    feat: create class for signup usecase

--- src/application.ts
+++ src/application.ts
@@ -1,31 +1,39 @@
 import crypto from "crypto";
-import pgp from "pg-promise";
 import { validateCpf } from "./validateCpf";
-import { getAccountByEmail, getAccountById, saveAccount } from './resource';
+import AccountDAO from './resource';
 
-export async function signup(input: any): Promise<any> {
-    const account = {
-        accountId: crypto.randomUUID(),
-        name: input.name,
-        email: input.email,
-        cpf: input.cpf,
-        carPlate: input.carPlate,
-        isPassenger: input.isPassenger,
-        isDriver: input.isDriver
-    };
-    const existingAccount = await getAccountByEmail(input.email);
-    if (existingAccount) throw new Error('Account already exists');
-    if (!input.name.match(/[a-zA-Z] [a-zA-Z]+/)) throw new Error('Invalid name');
-    if (!input.email.match(/^(.+)@(.+)$/)) throw new Error('Invalid email');
-    if (!validateCpf(input.cpf)) throw new Error('Invalid cpf');
-    if (input.isDriver && !input.carPlate.match(/[A-Z]{3}[0-9]{4}/)) throw new Error('Invalid car plate');
-    await saveAccount(account);
-    return {
-        accountId: account.accountId
-    };
+export default class AccountService {
+    accountDAO: AccountDAO
+    constructor() {
+        this.accountDAO = new AccountDAO();
+    }
+
+    async signup(input: any): Promise<any> {
+        const account = {
+            accountId: crypto.randomUUID(),
+            name: input.name,
+            email: input.email,
+            cpf: input.cpf,
+            carPlate: input.carPlate,
+            isPassenger: input.isPassenger,
+            isDriver: input.isDriver
+        };
+        const existingAccount = await this.accountDAO.getAccountByEmail(input.email);
+        if (existingAccount) throw new Error('Account already exists');
+        if (!input.name.match(/[a-zA-Z] [a-zA-Z]+/)) throw new Error('Invalid name');
+        if (!input.email.match(/^(.+)@(.+)$/)) throw new Error('Invalid email');
+        if (!validateCpf(input.cpf)) throw new Error('Invalid cpf');
+        if (input.isDriver && !input.carPlate.match(/[A-Z]{3}[0-9]{4}/)) throw new Error('Invalid car plate');
+        await this.accountDAO.saveAccount(account);
+        return {
+            accountId: account.accountId
+        };
+    }
+
+    async getAccount(accountId: string): Promise<any> {
+        const account = await this.accountDAO.getAccountById(accountId);
+        return account;
+    }
 }
 
-export async function getAccount(accountId: string): Promise<any> {
-    const account = await getAccountById(accountId);
-    return account;
-}
+
