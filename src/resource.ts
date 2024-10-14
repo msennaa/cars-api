142c04f137ce401393d11a97341132c39e8935ee
Matheus Sena <senamatheusjob@gmail.com>
2024-10-14 13:19:47 -0300

    feat: create accountDAO class

--- src/resource.ts
+++ src/resource.ts
@@ -1,22 +1,26 @@
 import pgp from "pg-promise";
 
-export async function getAccountByEmail(email: string) {
-    const connection = pgp()("postgres://postgres:123456@localhost:5432/app");
-    const [account] = await connection.query("select * from cccat17.account where email = $1", [email]);
-    await connection.$pool.end();
-    return account;
+export default class AccountDAO {
+    async getAccountByEmail(email: string) {
+        const connection = pgp()("postgres://postgres:123456@localhost:5432/app");
+        const [account] = await connection.query("select * from cccat17.account where email = $1", [email]);
+        await connection.$pool.end();
+        return account;
+    }
+
+    async getAccountById(accountId: string) {
+        const connection = pgp()("postgres://postgres:123456@localhost:5432/app");
+        const [account] = await connection.query("select * from cccat17.account where account_id = $1", [accountId]);
+        await connection.$pool.end();
+        return account;
+    }
+
+    async saveAccount(account: any) {
+        const connection = pgp()("postgres://postgres:123456@localhost:5432/app");
+        await connection.query("insert into cccat17.account (account_id, name, email, cpf, car_plate, is_passenger, is_driver) values ($1, $2, $3, $4, $5, $6, $7)", [account.accountId, account.name, account.email, account.cpf, account.carPlate, !!account.isPassenger, !!account.isDriver]);
+        await connection.$pool.end();
+    }
 }
 
-export async function getAccountById(accountId: string) {
-    const connection = pgp()("postgres://postgres:123456@localhost:5432/app");
-    const [account] = await connection.query("select * from cccat17.account where account_id = $1", [accountId]);
-    await connection.$pool.end();
-    return account;
-}
 
-export async function saveAccount(account: any) {
-    const connection = pgp()("postgres://postgres:123456@localhost:5432/app");
-    await connection.query("insert into cccat17.account (account_id, name, email, cpf, car_plate, is_passenger, is_driver) values ($1, $2, $3, $4, $5, $6, $7)", [account.accountId, account.name, account.email, account.cpf, account.carPlate, !!account.isPassenger, !!account.isDriver]);
-    await connection.$pool.end();
-}
 
