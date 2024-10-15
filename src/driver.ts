5c3bd306aec2fdfb6985ff7ce3cb75c0f0dc531a
Matheus Sena <senamatheusjob@gmail.com>
2024-10-15 05:11:35 -0300

    feat: create an API class

--- src/driver.ts
+++ src/driver.ts
@@ -1,28 +1,40 @@
 import express from 'express';
-import AccountService from './application';
+import AccountService, { AccountServiceProduction } from './application';
 import { AccountDAODatabase } from './resource';
-const app = express();
-app.use(express.json());
-const accountDAO = new AccountDAODatabase;
-const accountService = new AccountService(accountDAO);
 
-app.post("/signup", async function (req, res) {
-    const input = req.body;
-    try {
-        const output = await accountService.signup(input);
-        res.json(output);
-    } catch (error: any) {
-        res.status(422).json({
-            message: error.message
+
+export default class API {
+    app: any;
+    accountService: AccountService;
+
+    constructor(accountService: AccountService) {
+        this.app = express();
+        this.app.use(express.json());
+        this.accountService = accountService;
+    }
+
+    build() {
+        const accountDAO = new AccountDAODatabase;
+        const accountService = new AccountServiceProduction(accountDAO);
+        this.app.post("/signup", async (req: any, res: any) => {
+            const input = req.body;
+            try {
+                const output = await this.accountService.signup(input);
+                res.json(output);
+            } catch (error: any) {
+                res.status(422).json({
+                    message: error.message
+                })
+            }
+        })
+        this.app.get('/accounts/:accountId', async (req: any, res: any) => {
+            const accountId = req.params.accountId;
+            const output = await accountService.getAccount(accountId);
+            res.json(output);
         })
     }
-})
 
-
-app.get('/accounts/:accountId', async function (req, res) {
-    const accountId = req.params.accountId;
-    const output = await accountService.getAccount(accountId);
-    res.json(output);
-})
-
-app.listen(3000);
+    start() {
+        this.app.listen(3000);
+    }
+}
