2552f49d44550a83cdc0c37f40fbffc4b38c2639
Matheus Sena <senamatheusjob@gmail.com>
2024-10-15 04:56:29 -0300

    feat: AccountDAO became an interface and create AccountDAO memory and AccountDAODatabase to invert dependencies

--- src/resource.ts
+++ src/resource.ts
@@ -1,6 +1,12 @@
 import pgp from "pg-promise";
 
-export default class AccountDAO {
+export default interface AccountDAO {
+    getAccountByEmail(email: string): Promise<any>;
+    getAccountById(accountId: string): Promise<any>;
+    saveAccount(account: any): Promise<void>;
+}
+
+export class AccountDAODatabase implements AccountDAO {
     async getAccountByEmail(email: string) {
         const connection = pgp()("postgres://postgres:123456@localhost:5432/app");
         const [account] = await connection.query("select * from cccat17.account where email = $1", [email]);
@@ -19,6 +25,26 @@ export default class AccountDAO {
         const connection = pgp()("postgres://postgres:123456@localhost:5432/app");
         await connection.query("insert into cccat17.account (account_id, name, email, cpf, car_plate, is_passenger, is_driver) values ($1, $2, $3, $4, $5, $6, $7)", [account.accountId, account.name, account.email, account.cpf, account.carPlate, !!account.isPassenger, !!account.isDriver]);
         await connection.$pool.end();
+    }
+}
+
+export class AccountDAOMemory implements AccountDAO {
+    accounts: any[];
+
+    constructor() {
+        this.accounts = [];
+    }
+
+    async getAccountByEmail(email: string): Promise<any> {
+        return this.accounts.find((account: any) => account.email === email);
+    }
+
+    async getAccountById(accountId: string): Promise<any> {
+        return this.accounts.find((account: any) => account.accountId === accountId);
+    }
+
+    async saveAccount(account: any): Promise<void> {
+        this.accounts.push(account);
     }
 }
 
