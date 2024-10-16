5e4861cde7aa94e0fe038623955e7cef6965ab6c
Matheus Sena <senamatheusjob@gmail.com>
2024-10-16 08:19:26 -0300

    fix: tests for Cpf class

---
+++ test/Cpf.test.ts
@@ -0,0 +1,20 @@
+import Cpf from '../src/Cpf';
+
+test.each([
+    "97456321558",
+    "71428793860",
+    "87748248800"
+])("Deve testar se o cpf é válido %s", function (cpf: string) {
+    expect(new Cpf(cpf)).toBeDefined();
+});
+
+test.each([
+    "",
+    null,
+    undefined,
+    "123456",
+    "12345678901234567890",
+    "11111111111"
+])("Deve testar se o cpf é inválido %s", function (cpf: any) {
+    expect(() => new Cpf(cpf)).toThrowError('Invalid cpf')
+});
