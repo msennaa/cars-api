d00d26074399120aecd4b76bac4ef01f00ee1aa6
Matheus Sena <senamatheusjob@gmail.com>
2024-10-16 08:16:17 -0300

    fix: all tests

--- test/application.test.ts
+++ test/application.test.ts
@@ -1,14 +1,16 @@
 import GetAccount from '../src/GetAccount';
-import { AccountDAOMemory } from '../src/resource';
+import { AccountRepositoryDatabase } from '../src/AccountRepository';
 import SignUp from '../src/SignUp';
+import sinon from 'sinon';
+import Account from '../src/Account';
 
 let signUp: SignUp;
 let getAccount: GetAccount;
 
 beforeEach(() => {
-    const accountDAO = new AccountDAOMemory;
-    signUp = new SignUp(accountDAO);
-    getAccount = new GetAccount(accountDAO);
+    const accountRepository = new AccountRepositoryDatabase();
+    signUp = new SignUp(accountRepository);
+    getAccount = new GetAccount(accountRepository);
 })
 
 test('should create a passenger new account successfully', async () => {
@@ -100,3 +102,33 @@ test('should not create a driver new account with invalid car plate', async () =
     const promise = signUp.execute(input)
     expect(promise).rejects.toThrowError('Invalid car plate');
 })
+
+test('should create a passenger account with AccountRepository stub', async function () {
+    const email = `any_email${Math.random()}@mail.com`
+    const inputSignUp = {
+        name: 'any name',
+        email,
+        cpf: '97456321558',
+        isPassenger: true
+    }
+    const inputSignUpStub = Account.create(
+        'any name',
+        email,
+        '97456321558',
+        '',
+        true,
+        false
+    )
+    const stubSaveAccount = sinon.stub(AccountRepositoryDatabase.prototype, 'saveAccount').resolves();
+    const stubGetAccountByEmail = sinon.stub(AccountRepositoryDatabase.prototype, 'getAccountByEmail').resolves(undefined);
+    const stubGetAccountById = sinon.stub(AccountRepositoryDatabase.prototype, 'getAccountById').resolves(inputSignUpStub);
+    const outputSignUp = await signUp.execute(inputSignUp);
+    expect(outputSignUp.accountId).toBeDefined();
+    const outputGetAccount = await getAccount.execute(outputSignUp.accountId);
+    expect(outputGetAccount.name).toBe(inputSignUp.name)
+    expect(outputGetAccount.email).toBe(inputSignUp.email)
+    expect(outputGetAccount.cpf).toBe(inputSignUp.cpf)
+    stubSaveAccount.restore();
+    stubGetAccountByEmail.restore();
+    stubGetAccountById.restore();
+});
