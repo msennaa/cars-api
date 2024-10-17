b06a1a07372d3f2d990a01c66aff6697ca07ec5a
Matheus Sena <senamatheusjob@gmail.com>
2024-10-17 05:10:01 -0300

    feat: make account controller

---
+++ src/AccountController.ts
@@ -0,0 +1,19 @@
+import SignUp from './SignUp';
+import GetAccount from './GetAccount';
+import HttpServer from './HttpServer';
+
+export default class AccountController {
+    constructor(readonly httpServer: HttpServer, readonly signup: SignUp, readonly getAccount: GetAccount) {
+        this.httpServer.register('post', "/signup", async (params: any, body: any) => {
+            const input = body;
+            const output = await this.signup.execute(input);
+            return output;
+        })
+
+        this.httpServer.register('get', '/accounts/:{accountId}', async (params: any, body: any) => {
+            const accountId = params.accountId;
+            const output = await this.getAccount.execute(accountId);
+            return output;
+        })
+    }
+}
