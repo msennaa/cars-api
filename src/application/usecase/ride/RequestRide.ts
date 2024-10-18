351a95ec61fed9989b96d794be95168806525234
Matheus Sena <senamatheusjob@gmail.com>
2024-10-18 06:43:59 -0300

    feat: should not request a ride if accounts don't belongs a passenger

--- src/application/usecase/ride/RequestRide.ts
+++ src/application/usecase/ride/RequestRide.ts
@@ -1,13 +1,16 @@
 import Ride from '../../../domain/Ride';
+import AccountRepository from '../../repository/AccountRepository';
 import RideRepository from '../../repository/RideRepository';
 import UseCase from '../UseCase';
 
 export default class RequestRide implements UseCase {
 
-    constructor(readonly rideRepository: RideRepository) {
+    constructor(readonly rideRepository: RideRepository, readonly accountRepository: AccountRepository) {
     }
 
     async execute(input: Input): Promise<Output> {
+        const account = await this.accountRepository.getAccountById(input.passengerId);
+        if (!account.isPassenger) throw new Error('This account is not from a passenger');
         const ride = Ride.create(input.passengerId, input.fromLat, input.fromLong, input.toLat, input.toLong);
         await this.rideRepository.saveRide(ride);
         return {
