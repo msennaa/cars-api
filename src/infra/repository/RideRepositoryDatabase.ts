45dc4986609c5f67af0ec3f9fdc54d1ac47babe1
Matheus Sena <senamatheusjob@gmail.com>
2024-10-18 07:02:35 -0300

    feat: method to check if passenger has a active ride

--- src/infra/repository/RideRepositoryDatabase.ts
+++ src/infra/repository/RideRepositoryDatabase.ts
@@ -3,10 +3,7 @@ import Ride from '../../domain/Ride';
 import DatabaseConnection from '../database/DatabaseConnection';
 
 export default class RideRepositoryDatabase implements RideRepository {
-    constructor(readonly connection: DatabaseConnection) {
-
-    }
-
+    constructor(readonly connection: DatabaseConnection) { }
 
     async saveRide(ride: Ride): Promise<void> {
         await this.connection.query("insert into ride (ride_id, passenger_id, driver_id, from_lat, from_long, to_lat, to_long, status, date) values ($1, $2, $3, $4, $5, $6, $7, $8, $9)", [ride.rideId, ride.passengerId, ride.driverId || null, ride.fromLat, ride.fromLong, ride.toLat, ride.toLong, ride.status, ride.date])
@@ -18,4 +15,8 @@ export default class RideRepositoryDatabase implements RideRepository {
         return new Ride(rideData.ride_id, rideData.passenger_id, rideData.driver_id, parseFloat(rideData.from_lat), parseFloat(rideData.from_long), parseFloat(rideData.to_lat), parseFloat(rideData.to_long), rideData.status, rideData.date);
     }
 
+    async hasActiveRideByPassengerId(passengerId: string): Promise<boolean> {
+        const [rideData] = await this.connection.query("select count(*)::int as count from ride where passenger_id = $1 and status in ('requested', 'accepted', 'in_progress')", [passengerId])
+        return rideData.count > 0;
+    }
 }
