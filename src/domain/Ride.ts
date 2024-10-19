5249bf44d2e6c99b48d1167097aeb6b223d4c3b4
Matheus Sena <senamatheusjob@gmail.com>
2024-10-19 08:29:49 -0300

    feat: Create Ride value object

--- src/domain/Ride.ts
+++ src/domain/Ride.ts
@@ -1,8 +1,13 @@
 import crypto from 'crypto';
+import Coord from './Coord';
 
 export default class Ride {
-    constructor(readonly rideId: string, readonly passengerId: string, readonly driverId: string, readonly fromLat: number, readonly fromLong: number, readonly toLat: number, readonly toLong: number, readonly status: string, readonly date: Date) {
+    private from: Coord;
+    private to: Coord;
 
+    constructor(readonly rideId: string, readonly passengerId: string, readonly driverId: string, fromLat: number, fromLong: number, toLat: number, toLong: number, readonly status: string, readonly date: Date) {
+        this.from = new Coord(fromLat, fromLong);
+        this.to = new Coord(toLat, toLong);
     }
 
     static create(passengerId: string, fromLat: number, fromLong: number, toLat: number, toLong: number) {
@@ -11,4 +16,12 @@ export default class Ride {
         const date = new Date();
         return new Ride(rideId, passengerId, '', fromLat, fromLong, toLat, toLong, status, date)
     }
-}
+
+    getFrom() {
+        return this.from;
+    }
+
+    getTo() {
+        return this.to;
+    }
+} 
