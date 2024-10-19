2f99f5586777ae2a2d6c7b3cb2c4e9c28f5fef5b
Matheus Sena <senamatheusjob@gmail.com>
2024-10-19 08:45:14 -0300

    test: should test Coord value object

---
+++ test/unit/Coord.test.ts
@@ -0,0 +1,16 @@
+import Coord from '../../src/domain/Coord'
+
+test('should create a valid coordinate', function () {
+    const coord = new Coord(90, 180);
+    expect(coord).toBeDefined();
+    expect(coord.getLat()).toBe(90)
+    expect(coord.getLong()).toBe(180)
+})
+
+test('should not create a coordinate with invalid latitude', function () {
+    expect(() => new Coord(-180, 180)).toThrowError('Invalid latitude')
+})
+
+test('should not create a coordinate with invalid longitude', function () {
+    expect(() => new Coord(90, -270)).toThrowError('Invalid longitude')
+})
