bbb7f19fed4d24dd34e7eb4e75a79bc6417e87f7
Matheus Sena <senamatheusjob@gmail.com>
2024-10-20 21:52:20 -0300

    feat: add hasActiveRideByDriverId method and updateRide

--- src/infra/repository/RideRepositoryDatabase.ts
+++ src/infra/repository/RideRepositoryDatabase.ts
@@ -6,17 +6,26 @@ export default class RideRepositoryDatabase implements RideRepository {
     constructor(readonly connection: DatabaseConnection) { }
 
     async saveRide(ride: Ride): Promise<void> {
-        await this.connection.query("insert into ride (ride_id, passenger_id, driver_id, from_lat, from_long, to_lat, to_long, status, date) values ($1, $2, $3, $4, $5, $6, $7, $8, $9)", [ride.rideId, ride.passengerId, ride.driverId || null, ride.getFrom().getLat(), ride.getFrom().getLong(), ride.getTo().getLat(), ride.getTo().getLong(), ride.status, ride.date])
+        await this.connection.query("insert into ride (ride_id, passenger_id, driver_id, from_lat, from_long, to_lat, to_long, status, date, distance) values ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)", [ride.rideId, ride.passengerId, ride.driverId || null, ride.getFrom().getLat(), ride.getFrom().getLong(), ride.getTo().getLat(), ride.getTo().getLong(), ride.status, ride.date, ride.distance])
     }
 
     async getRideById(rideId: string): Promise<Ride> {
         const [rideData] = await this.connection.query('select * from ride where ride_id = $1', [rideId])
         if (!rideData) throw new Error();
-        return new Ride(rideData.ride_id, rideData.passenger_id, rideData.driver_id, parseFloat(rideData.from_lat), parseFloat(rideData.from_long), parseFloat(rideData.to_lat), parseFloat(rideData.to_long), rideData.status, rideData.date);
+        return new Ride(rideData.ride_id, rideData.passenger_id, rideData.driver_id, parseFloat(rideData.from_lat), parseFloat(rideData.from_long), parseFloat(rideData.to_lat), parseFloat(rideData.to_long), rideData.status, rideData.date, parseFloat(rideData.distance));
     }
 
     async hasActiveRideByPassengerId(passengerId: string): Promise<boolean> {
         const [rideData] = await this.connection.query("select count(*)::int as count from ride where passenger_id = $1 and status in ('requested', 'accepted', 'in_progress')", [passengerId])
         return rideData.count > 0;
+    }
+
+    async hasActiveRideByDriverId(driverId: string): Promise<boolean> {
+        const [rideData] = await this.connection.query("select count(*)::int as count from ride where driver_id = $1 and status in ('accepted', 'in_progress')", [driverId])
+        return rideData.count > 0;
+    }
+
+    async updateRide(ride: Ride): Promise<void> {
+        await this.connection.query("update ride set driver_id = $1, status = $2, distance = $3 where ride_id = $4", [ride.driverId, ride.status, ride.distance, ride.rideId]);
     }
 }
