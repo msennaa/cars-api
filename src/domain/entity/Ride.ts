be829e97787af711ce543780a18c70ea09ce18cf
Matheus Sena <senamatheusjob@gmail.com>
2024-10-20 21:52:00 -0300

    feat: add start and updatePosition methods

--- src/domain/entity/Ride.ts
+++ src/domain/entity/Ride.ts
@@ -1,11 +1,14 @@
 import crypto from 'crypto';
 import Coord from '../vo/Coord';
+import Account from './Account';
+import Position from './Position';
+import Segment from '../vo/Segment';
 
 export default class Ride {
     private from: Coord;
     private to: Coord;
 
-    constructor(readonly rideId: string, readonly passengerId: string, public driverId: string, fromLat: number, fromLong: number, toLat: number, toLong: number, public status: string, readonly date: Date) {
+    constructor(readonly rideId: string, readonly passengerId: string, public driverId: string, fromLat: number, fromLong: number, toLat: number, toLong: number, public status: string, readonly date: Date, public distance: number) {
         this.from = new Coord(fromLat, fromLong);
         this.to = new Coord(toLat, toLong);
     }
@@ -14,7 +17,8 @@ export default class Ride {
         const rideId = crypto.randomUUID();
         const status = 'requested';
         const date = new Date();
-        return new Ride(rideId, passengerId, '', fromLat, fromLong, toLat, toLong, status, date)
+        const distance = 0;
+        return new Ride(rideId, passengerId, '', fromLat, fromLong, toLat, toLong, status, date, distance)
     }
 
     getFrom() {
@@ -25,9 +29,22 @@ export default class Ride {
         return this.to;
     }
 
-    accept(driverId: string) {
-        if (status !== 'requested' || driverId) throw new Error('');
-        this.driverId = driverId;
+    accept(account: Account) {
+        if (!account.isDriver) throw new Error('Account is not from a driver');
+        if (this.status !== 'requested') throw new Error('Invalid status');
+        this.driverId = account.accountId;
         this.status = 'accepted';
+    }
+
+    start() {
+        if (this.status !== 'accepted') throw new Error('Invalid status')
+        this.status = 'in_progress'
+    }
+
+    updatePosition(lastPosition: Position, currentPosition: Position) {
+        if (this.status !== 'in_progress') throw new Error('Invalid status')
+        const segment = new Segment(lastPosition.coord, currentPosition.coord);
+        this.distance += segment.getDistance();
+
     }
 } 
