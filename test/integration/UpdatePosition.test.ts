25812eab28487f07e5cafaae620f94c2659d4c58
Matheus Sena <senamatheusjob@gmail.com>
2024-10-21 06:28:59 -0300

    test: should calculate fare during night and on sunday

--- test/integration/UpdatePosition.test.ts
+++ test/integration/UpdatePosition.test.ts
@@ -34,7 +34,7 @@ beforeEach(() => {
     updatePosition = new UpdatePosition(rideRepository, positionRepository);
 })
 
-test.only('Should update position from a ride', async function () {
+test('Should update position from a ride during day', async function () {
     const inputPassenger = {
         name: 'any name',
         email: `any_email${Math.random()}@mail.com`,
@@ -71,12 +71,14 @@ test.only('Should update position from a ride', async function () {
         rideId: outputRequestRide.rideId,
         lat: -27.584905257808835,
         long: -48.545022195325124,
+        date: new Date('2023-03-01T10:00:00')
     }
 
     const secondUpdatePosition = {
         rideId: outputRequestRide.rideId,
         lat: -27.496887588317275,
-        long: -48.522234807851476
+        long: -48.522234807851476,
+        date: new Date('2023-03-01T10:10:00')
     }
     await updatePosition.execute(firstUpdatePosition);
     await updatePosition.execute(secondUpdatePosition);
@@ -85,6 +87,115 @@ test.only('Should update position from a ride', async function () {
     expect(outputGetRide.currentLong).toBe(-48.522234807851476);
     expect(outputGetRide.distance).toBe(10);
     expect(outputGetRide.fare).toBe(21);
+})
+
+test('Should update position from a ride during the night', async function () {
+    const inputPassenger = {
+        name: 'any name',
+        email: `any_email${Math.random()}@mail.com`,
+        cpf: '97456321558',
+        isPassenger: true
+    }
+    const outputSignupPassenger = await signUp.execute(inputPassenger)
+    const inputRequestRide = {
+        passengerId: outputSignupPassenger.accountId,
+        fromLat: -27.584905257808835,
+        fromLong: -48.545022195325124,
+        toLat: -27.496887588317275,
+        toLong: -48.522234807851476
+    }
+    const outputRequestRide = await requestRide.execute(inputRequestRide);
+    const inputDriver = {
+        name: 'any name',
+        email: `any_email${Math.random()}@mail.com`,
+        cpf: '97456321558',
+        carPlate: 'AAA9999',
+        isDriver: true
+    }
+    const outputSignupDriver = await signUp.execute(inputDriver)
+    const inputAcceptRide = {
+        rideId: outputRequestRide.rideId,
+        driverId: outputSignupDriver.accountId,
+    }
+    await acceptRide.execute(inputAcceptRide)
+    const inputStartRide = {
+        rideId: outputRequestRide.rideId,
+    }
+    await startRide.execute(inputStartRide);
+    const firstUpdatePosition = {
+        rideId: outputRequestRide.rideId,
+        lat: -27.584905257808835,
+        long: -48.545022195325124,
+        date: new Date('2023-03-01T23:00:00')
+    }
+
+    const secondUpdatePosition = {
+        rideId: outputRequestRide.rideId,
+        lat: -27.496887588317275,
+        long: -48.522234807851476,
+        date: new Date('2023-03-01T23:10:00')
+    }
+    await updatePosition.execute(firstUpdatePosition);
+    await updatePosition.execute(secondUpdatePosition);
+    const outputGetRide = await getRide.execute(outputRequestRide.rideId);
+    expect(outputGetRide.currentLat).toBe(-27.496887588317275);
+    expect(outputGetRide.currentLong).toBe(-48.522234807851476);
+    expect(outputGetRide.distance).toBe(10);
+    expect(outputGetRide.fare).toBe(39);
+})
+
+test('Should update position from a ride on sunday', async function () {
+    const inputPassenger = {
+        name: 'any name',
+        email: `any_email${Math.random()}@mail.com`,
+        cpf: '97456321558',
+        isPassenger: true
+    }
+    const outputSignupPassenger = await signUp.execute(inputPassenger)
+    const inputRequestRide = {
+        passengerId: outputSignupPassenger.accountId,
+        fromLat: -27.584905257808835,
+        fromLong: -48.545022195325124,
+        toLat: -27.496887588317275,
+        toLong: -48.522234807851476
+    }
+    const outputRequestRide = await requestRide.execute(inputRequestRide);
+    const inputDriver = {
+        name: 'any name',
+        email: `any_email${Math.random()}@mail.com`,
+        cpf: '97456321558',
+        carPlate: 'AAA9999',
+        isDriver: true
+    }
+    const outputSignupDriver = await signUp.execute(inputDriver)
+    const inputAcceptRide = {
+        rideId: outputRequestRide.rideId,
+        driverId: outputSignupDriver.accountId,
+    }
+    await acceptRide.execute(inputAcceptRide)
+    const inputStartRide = {
+        rideId: outputRequestRide.rideId,
+    }
+    await startRide.execute(inputStartRide);
+    const firstUpdatePosition = {
+        rideId: outputRequestRide.rideId,
+        lat: -27.584905257808835,
+        long: -48.545022195325124,
+        date: new Date('2021-03-07T10:00:00')
+    }
+    const secondUpdatePosition = {
+        rideId: outputRequestRide.rideId,
+        lat: -27.496887588317275,
+        long: -48.522234807851476,
+        date: new Date('2021-03-07T10:10:00')
+    }
+    await updatePosition.execute(firstUpdatePosition);
+    await updatePosition.execute(secondUpdatePosition);
+    const outputGetRide = await getRide.execute(outputRequestRide.rideId);
+    expect(outputGetRide.currentLat).toBe(-27.496887588317275);
+    expect(outputGetRide.currentLong).toBe(-48.522234807851476);
+    expect(outputGetRide.distance).toBe(10);
+    expect(outputGetRide.fare).toBe(50);
 })
 
 afterEach(async () => {
