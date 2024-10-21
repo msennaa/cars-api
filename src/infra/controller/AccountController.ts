03f7fccfb06123aecaa7f18f71c9b1dcc3c4978b
Matheus Sena <senamatheusjob@gmail.com>
2024-10-21 18:57:31 -0300

    refactor: implements inject decorator

--- src/infra/controller/AccountController.ts
+++ src/infra/controller/AccountController.ts
@@ -1,18 +1,26 @@
 import GetAccount from '../../application/usecase/account/GetAccount';
 import SignUp from '../../application/usecase/account/SignUp';
+import { inject } from '../di/Registry';
 import HttpServer from '../http/HttpServer';
 
 export default class AccountController {
-    constructor(readonly httpServer: HttpServer, readonly signup: SignUp, readonly getAccount: GetAccount) {
-        this.httpServer.register('post', "/signup", async (params: any, body: any) => {
+    @inject('httpServer')
+    httpServer?: HttpServer;
+    @inject('signup')
+    signup?: SignUp;
+    @inject('getAccount')
+    getAccount?: GetAccount;
+
+    constructor() {
+        this.httpServer?.register('post', "/signup", async (params: any, body: any) => {
             const input = body;
-            const output = await this.signup.execute(input);
+            const output = await this.signup?.execute(input);
             return output;
         })
 
-        this.httpServer.register('get', '/accounts/:{accountId}', async (params: any, body: any) => {
+        this.httpServer?.register('get', '/accounts/:{accountId}', async (params: any, body: any) => {
             const accountId = params.accountId;
-            const output = await this.getAccount.execute(accountId);
+            const output = await this.getAccount?.execute(accountId);
             return output;
         })
     }
--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import SignUp from './application/usecase/account/SignUp';
 import { PgPromiseAdapter } from './infra/database/DatabaseConnection';
 import { ExpressAdapter, HapiAdapter } from './infra/http/HttpServer';
 import MailerGatewayFake from './infra/gateway/MailerGatewayFake';
+import Registry from './infra/di/Registry';
 
 const connection = new PgPromiseAdapter();
 const accountRepository = new AccountRepositoryDatabase(connection);
@@ -13,5 +14,8 @@ const signup = new SignUp(accountRepository, mailerGateway);
 const getAccount = new GetAccount(accountRepository);
 const httpServer = new ExpressAdapter();
 // const httpServer = new HapiAdapter();
-new AccountController(httpServer, signup, getAccount);
+Registry.getInstance().provide('httpServer', httpServer);
+Registry.getInstance().provide('signup', signup)
+Registry.getInstance().provide('getAccount', getAccount);
+new AccountController();
 httpServer.listen(3000)
