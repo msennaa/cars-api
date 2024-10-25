71d4a7fdc3832664a12f21658e50bb400a6f7975
Matheus Sena <senamatheusjob@gmail.com>
2024-10-24 21:34:47 -0300

    feat: generate invoice

---
+++ backend/ride/src/application/usecase/invoice/GenerateInvoice.ts
@@ -0,0 +1,14 @@
+export default class GenerateInvoice {
+    constructor() {
+
+    }
+
+    async execute(input: Input): Promise<void> {
+        console.log('generateInvoice', input);
+    }
+}
+
+type Input = {
+    rideId: string,
+    amount: number,
+}
