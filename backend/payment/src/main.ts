0d792e485da1129a5cf99032796580533b5bc054
Matheus Sena <senamatheusjob@gmail.com>
2024-10-25 23:34:52 -0300

    feat: implements ride on payment bounded content

--- backend/payment/src/main.ts
+++ backend/payment/src/main.ts
@@ -3,10 +3,19 @@ import { ExpressAdapter, HapiAdapter } from './infra/http/HttpServer';
 import Registry from './infra/di/Registry';
 import ProcessPayment from './application/usecase/payment/ProcessPayment';
 import PaymentController from './infra/controller/PaymentController';
+import { RabbitMQAdapter } from './infra/queue/Queue';
 
-const connection = new PgPromiseAdapter();
-const httpServer = new ExpressAdapter();
-// const httpServer = new HapiAdapter();
-const processPayment = new ProcessPayment();
-new PaymentController(httpServer, processPayment)
-httpServer.listen(3002)
+(async () => {
+    const connection = new PgPromiseAdapter();
+    const httpServer = new ExpressAdapter();
+    // const httpServer = new HapiAdapter();
+    const processPayment = new ProcessPayment();
+    new PaymentController(httpServer, processPayment)
+    const queue = new RabbitMQAdapter();
+    await queue.connect();
+    queue.consume('rideCompleted.processPayment', async (input: any) => {
+        await processPayment.execute(input);
+    })
+    httpServer.listen(3002)
+})();
+
