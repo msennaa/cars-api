b669e3c3e5edd953d7dd3d59b84ec57b3845fdc0
Matheus Sena <senamatheusjob@gmail.com>
2024-10-26 00:15:11 -0300

    feat: create fastify adapter

--- backend/account/src/infra/http/HttpServer.ts
+++ backend/account/src/infra/http/HttpServer.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import Hapi from '@hapi/hapi';
 import HyperExpress from 'hyper-express';
+import Fastify, { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify'
 
 export default interface HttpServer {
     register(method: string, url: string, callback: Function): void;
@@ -91,3 +92,31 @@ export class HyperExpressAdapter implements HttpServer {
         });
     }
 }
+
+export class FastifyAdapter implements HttpServer {
+    app: FastifyInstance;
+
+    constructor() {
+        this.app = Fastify();
+    }
+
+    register(method: string, url: string, callback: Function): void {
+        (this.app as any)[method](url.replace(/\{|\}/g, ''), async (request: FastifyRequest, reply: FastifyReply) => {
+            try {
+                const { params, body } = request;
+                const output = await callback(params, body);
+                reply.send(output);
+            } catch (error: any) {
+                reply.status(422).send({
+                    message: error.message,
+                });
+            }
+        });
+    }
+
+    listen(port: number): void {
+        this.app.listen({ port });
+
+    }
+}
+
