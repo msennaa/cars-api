7455193b504bccb8e85fd840222e4b23d01d95f3
Matheus Sena <senamatheusjob@gmail.com>
2024-10-25 23:58:29 -0300

    feat: create hyper-express adapter

--- backend/account/src/infra/http/HttpServer.ts
+++ backend/account/src/infra/http/HttpServer.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import Hapi from '@hapi/hapi';
+import HyperExpress from 'hyper-express';
 
 export default interface HttpServer {
     register(method: string, url: string, callback: Function): void;
@@ -58,5 +59,35 @@ export class HapiAdapter implements HttpServer {
         this.server.settings.port = port;
         this.server.start();
     }
+}
 
+export class HyperExpressAdapter implements HttpServer {
+    app: HyperExpress.Server;
+
+    constructor() {
+        this.app = new HyperExpress.Server();
+    }
+
+    register(method: string, url: string, callback: Function): void {
+        (this.app as any)[method](url.replace(/\{|\}/g, ''), async (req: any, res: any) => {
+            try {
+                const params = req.params;
+                const body = await req.json();
+                const output = await callback(params, body);
+                res.json(output);
+            } catch (error: any) {
+                res.status(422).json({
+                    message: error.message
+                });
+            }
+        });
+    }
+
+    listen(port: number): void {
+        this.app.listen(port).then(() => {
+            console.log(`Server is running on http://localhost:${port}`);
+        }).catch((error) => {
+            console.error(`Failed to start server: ${error.message}`);
+        });
+    }
 }
