77dc3f406ebd53706c259942d7daa2a8b4532ae0
Matheus Sena <senamatheusjob@gmail.com>
2024-10-25 23:24:51 -0300

    Feat: implements queue on finish ride method

--- backend/ride/src/application/usecase/ride/FinishRide.ts
+++ backend/ride/src/application/usecase/ride/FinishRide.ts
@@ -1,20 +1,21 @@
 import RideCompletedEvent from '../../../domain/event/RideCompletedEvent';
 import Mediator from '../../../infra/mediator/Mediator';
-import AccountGateway from '../../gateway/AccountGateway';
+import Queue from '../../../infra/queue/Queue';
 import PaymentGateway from '../../gateway/PaymentGateway';
 import RideRepository from '../../repository/RideRepository';
 import UseCase from '../UseCase';
 
 export default class FinishRide implements UseCase {
 
-    constructor(readonly rideRepository: RideRepository, readonly mediator: Mediator, readonly paymentGateway: PaymentGateway) {
+    constructor(readonly rideRepository: RideRepository, readonly mediator: Mediator, readonly paymentGateway: PaymentGateway, readonly queue: Queue) {
     }
 
     async execute(input: Input): Promise<void> {
         const ride = await this.rideRepository.getRideById(input.rideId)
         ride.register('rideCompleted', async (event: RideCompletedEvent) => {
             // this.mediator.notify('rideCompleted', event)
-            await this.paymentGateway.processPayment(event);
+            // await this.paymentGateway.processPayment(event);
+            await this.queue.publish('rideCompleted', event);
         })
         ride.finish();
         await this.rideRepository.updateRide(ride);
