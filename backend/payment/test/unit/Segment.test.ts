0492821a6f6d6abd69bb5e07adb30470f7181430
Matheus Sena <senamatheusjob@gmail.com>
2024-10-27 17:56:19 -0300

    refactor: remove useless tests

--- backend/payment/test/unit/Segment.test.ts
+++
@@ -1,9 +0,0 @@
-import Coord from '../../src/domain/vo/Coord';
-import Segment from '../../src/domain/vo/Segment';
-
-test('Should calculate the segment distance', function () {
-    const from = new Coord(-27.584905257808835, -48.545022195325124);
-    const to = new Coord(-27.496887588317275, -48.522234807851476);
-    const segment = new Segment(from, to)
-    expect(segment.getDistance()).toBe(10);
-})
