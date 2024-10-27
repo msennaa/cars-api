b30a83d7694483e83ad72f179cc41119188fe8f5
Matheus Sena <senamatheusjob@gmail.com>
2024-10-27 16:52:33 -0300

    feat: create ride controller

---
+++ backend/ride/src/infra/controller/RideController.ts
@@ -0,0 +1,12 @@
+import RequestRide from '../../application/usecase/ride/RequestRide';
+import HttpServer from '../http/HttpServer';
+import Queue from '../queue/Queue';
+
+export default class RideController {
+    constructor(readonly httpServer: HttpServer, readonly requestRide: RequestRide, readonly queue: Queue) {
+        httpServer.register('post', '/request_ride', async (params: any, body: any) => {
+            const response = await requestRide.execute(body);
+            return response;
+        })
+    }
+}
