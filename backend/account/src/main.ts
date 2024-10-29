2a9ca5266b93865054ac37f020eb21e86de3d1f5
Matheus Sena <senamatheusjob@gmail.com>
2024-10-28 22:29:44 -0300

    setup: queue on account bounded context

--- backend/account/src/main.ts
+++ backend/account/src/main.ts
@@ -6,18 +6,28 @@ import { PgPromiseAdapter } from './infra/database/DatabaseConnection';
 import { ExpressAdapter, FastifyAdapter, HapiAdapter, HyperExpressAdapter } from './infra/http/HttpServer';
 import MailerGatewayFake from './infra/gateway/MailerGatewayFake';
 import Registry from './infra/di/Registry';
+import { RabbitMQAdapter } from './infra/queue/Queue';
+import QueueController from './infra/controller/QueueController';
 
-const connection = new PgPromiseAdapter();
-const accountRepository = new AccountRepositoryDatabase(connection);
-const mailerGateway = new MailerGatewayFake();
-const signup = new SignUp(accountRepository, mailerGateway);
-const getAccount = new GetAccount(accountRepository);
-// const httpServer = new ExpressAdapter();
-// const httpServer = new HapiAdapter();
-// const httpServer = new HyperExpressAdapter();
-const httpServer = new FastifyAdapter();
-Registry.getInstance().provide('httpServer', httpServer);
-Registry.getInstance().provide('signup', signup)
-Registry.getInstance().provide('getAccount', getAccount);
-new AccountController();
-httpServer.listen(3001)
+(async () => {
+    const connection = new PgPromiseAdapter();
+    const accountRepository = new AccountRepositoryDatabase(connection);
+    const mailerGateway = new MailerGatewayFake();
+    const signup = new SignUp(accountRepository, mailerGateway);
+    const getAccount = new GetAccount(accountRepository);
+    // const httpServer = new ExpressAdapter();
+    // const httpServer = new HapiAdapter();
+    const httpServer = new HyperExpressAdapter();
+    // const httpServer = new FastifyAdapter();
+    const queue = new RabbitMQAdapter();
+    await queue.connect();
+    await queue.setup('signup', 'signup')
+    Registry.getInstance().provide('httpServer', httpServer);
+    Registry.getInstance().provide('signup', signup)
+    Registry.getInstance().provide('getAccount', getAccount);
+    Registry.getInstance().provide('queue', queue);
+    new AccountController();
+    new QueueController();
+    httpServer.listen(3001)
+})()
+
