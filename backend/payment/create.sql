f88da3230cdc931732c95322c9fb1a90db5ae090
Matheus Sena <senamatheusjob@gmail.com>
2024-10-28 22:32:20 -0300

    setup: initiate table create on postgresql

--- backend/payment/create.sql
+++ backend/payment/create.sql
@@ -1,21 +1,7 @@
-create table ride (
+create table transactions (
+    transaction_id uuid,
 	ride_id uuid,
-	passenger_id uuid,
-	driver_id uuid,
+	amount numeric,
 	status text,
-	fare numeric,
-	distance numeric,
-	from_lat numeric,
-	from_long numeric,
-	to_lat numeric,
-	to_long numeric,
-	date timestamp
-);
-
-create table position (
-	position_id uuid,
-	ride_id uuid,
-	lat numeric,
-	long numeric,
 	date timestamp
 );
