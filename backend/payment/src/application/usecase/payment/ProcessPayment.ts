db14584571110db84b50f54c8521426dbbfb8ef7
Matheus Sena <senamatheusjob@gmail.com>
2024-10-28 22:30:48 -0300

    fix: return of ProcessPayment

--- backend/payment/src/application/usecase/payment/ProcessPayment.ts
+++ backend/payment/src/application/usecase/payment/ProcessPayment.ts
@@ -1,14 +1,26 @@
+import Transaction from '../../../domain/entity/Transaction';
+import TransactionRepository from '../../repository/TransactionRepository';
+
 export default class ProcessPayment {
-    constructor() {
+    constructor(readonly transactionRepository: TransactionRepository) {
 
     }
 
-    async execute(input: Input): Promise<void> {
+    async execute(input: Input): Promise<Output> {
         console.log('process payment', input);
+        const transaction = Transaction.create(input.rideId, input.amount);
+        await this.transactionRepository.saveTransaction(transaction);
+        return {
+            transactionId: transaction.transactionId,
+        }
     }
 }
 
 type Input = {
     rideId: string,
     amount: number,
+}
+
+type Output = {
+    transactionId: string,
 }
