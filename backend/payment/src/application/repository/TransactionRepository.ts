690eb50777a04eafbb6133b27e8fe108cd0e49d2
Matheus Sena <senamatheusjob@gmail.com>
2024-10-28 22:31:36 -0300

    feat: create transaction repository

---
+++ backend/payment/src/application/repository/TransactionRepository.ts
@@ -0,0 +1,6 @@
+import Transaction from '../../domain/entity/Transaction';
+
+export default interface TransactionRepository {
+    saveTransaction(transaction: Transaction): Promise<void>;
+    getTransactionById(transactionId: string): Promise<Transaction | null>;
+}
