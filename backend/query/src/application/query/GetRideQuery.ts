4db063cfacc5611fd1b344e314a9c4c2eabc6f3e
Matheus Sena <senamatheusjob@gmail.com>
2024-10-31 23:22:00 -0300

    feat: Optimize GetRideQuery for improved performance

--- backend/query/src/application/query/GetRideQuery.ts
+++ backend/query/src/application/query/GetRideQuery.ts
@@ -5,16 +5,13 @@ export default class GetRideQuery {
         const connectionRide = new PgPromiseAdapter(5433);
         const connectionAccount = new PgPromiseAdapter(5432);
         const connectionPayment = new PgPromiseAdapter(5434);
-
         const [ride] = await connectionRide.query('SELECT * FROM ride WHERE ride_id = $1', rideId)
         const [passenger] = await connectionAccount.query('SELECT * FROM account WHERE account_id = $1', ride.passenger_id)
         const [driver] = await connectionAccount.query('SELECT * FROM account WHERE account_id = $1', ride.driver_id)
         const [payment] = await connectionPayment.query('SELECT * FROM transactions WHERE ride_id = $1', rideId)
-
         await connectionRide.close()
         await connectionAccount.close()
         await connectionPayment.close()
-
         return {
             ride,
             passenger,
