0f87bc5544ab9aa38428357112bfc7476391c78e
Matheus Sena <senamatheusjob@gmail.com>
2024-10-31 23:22:07 -0300

    test: Update GetRideQuery.test.ts for improved test coverage

--- backend/query/test/GetRideQuery.test.ts
+++ backend/query/test/GetRideQuery.test.ts
@@ -1,4 +1,5 @@
 import GetRideApiComposition from '../src/application/query/GetRideAPIComposition';
+import GetRideProjection from '../src/application/query/GetRideProjection';
 import GetRideQuery from '../src/application/query/GetRideQuery';
 import HttpClient, { AxiosAdapter } from '../src/infra/http/HttpClient';
 
@@ -9,15 +10,23 @@ beforeEach(() => {
 })
 
 test('Should get a finished ride with query', async function () {
-    const rideId = 'd54700ef-07f1-43b5-b097-1245dacce2c0';
+    const rideId = 'e6404b26-1567-4369-973c-8c5903dfaac3';
     const getRideQuery = new GetRideQuery();
     const output = await getRideQuery.execute(rideId);
-    console.log(output);
+    expect(output).toBeDefined()
 })
 
 test('Should get a finished ride with API composition', async function () {
-    const rideId = 'd54700ef-07f1-43b5-b097-1245dacce2c0';
+    const rideId = 'e6404b26-1567-4369-973c-8c5903dfaac3';
+    const getRideQuery = new GetRideProjection();
+    const output = await getRideQuery.execute(rideId);
+    expect(output).toBeDefined()
+})
+
+test('Should get a finished ride with API composition', async function () {
+    const rideId = 'e6404b26-1567-4369-973c-8c5903dfaac3';
     const getRideQuery = new GetRideApiComposition(httpClient);
     const output = await getRideQuery.execute(rideId);
-    console.log(output);
+    expect(output).toBeDefined()
 })
+
